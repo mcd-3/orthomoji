026094f64d428dcbb7194050a3ad2fa49506662c
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2021-10-30 00:57:10 -0400

    Added basic alphabet font data

--- fonts/orthomoji.js
+++ fonts/orthomoji.js
@@ -27,75 +27,167 @@ const font = {
         X,O,O,O,n,
         X,X,X,X,n
     ],
-    // "d": [
-
-    // ],
-    // "e": [
-
-    // ],
-    // "f": [
-
-    // ],
-    // "g": [
-
-    // ],
-    // "h": [
-
-    // ],
-    // "i": [
-
-    // ],
-    // "j": [
-
-    // ],
-    // "k": [
-
-    // ],
-    // "l": [
-
-    // ],
-    // "m": [
-
-    // ],
-    // "n": [
-
-    // ],
-    // "o": [
-
-    // ],
-    // "p": [
-
-    // ],
-    // "q": [
-
-    // ],
-    // "r": [
-
-    // ],
-    // "s": [
-
-    // ],
-    // "t": [
-
-    // ],
-    // "u": [
-
-    // ],
-    // "v": [
-
-    // ],
-    // "w": [
-
-    // ],
-    // "x": [
-
-    // ],
-    // "y": [
-
-    // ],
-    // "z": [
-
-    // ],
+    "d": [
+        X,X,X,O,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,X,X,O,n
+    ],
+    "e": [
+        X,X,X,X,n,
+        X,O,O,O,n,
+        X,X,X,X,n,
+        X,O,O,O,n,
+        X,X,X,X,n
+    ],
+    "e": [
+        X,X,X,X,n,
+        X,O,O,O,n,
+        X,X,X,X,n,
+        X,O,O,O,n,
+        X,O,O,O,n
+    ],
+    "g": [
+        X,X,X,X,n,
+        X,O,O,O,n,
+        X,O,X,X,n,
+        X,O,O,X,n,
+        X,X,X,X,n
+    ],
+    "h": [
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,X,X,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n
+    ],
+    "i": [
+        O,X,X,O,n,
+        O,X,X,O,n,
+        O,X,X,O,n,
+        O,X,X,O,n,
+        O,X,X,O,n
+    ],
+    "j": [
+        X,X,X,X,n,
+        O,O,X,O,n,
+        O,O,X,O,n,
+        O,O,X,O,n,
+        X,X,X,O,n
+    ],
+    "k": [
+        X,O,O,X,n,
+        X,O,X,O,n,
+        X,X,O,O,n,
+        X,O,X,O,n,
+        X,O,O,X,n
+    ],
+    "l": [
+        X,O,O,O,n,
+        X,O,O,O,n,
+        X,O,O,O,n,
+        X,O,O,O,n,
+        X,X,X,X,n
+    ],
+    "m": [
+        X,O,O,X,n,
+        X,X,X,X,n,
+        X,X,X,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n
+    ],
+    "n": [
+        X,O,O,X,n,
+        X,X,O,X,n,
+        X,X,X,X,n,
+        X,O,X,X,n,
+        X,O,O,X,n
+    ],
+    "o": [
+        X,X,X,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,X,X,X,n
+    ],
+    "p": [
+        X,X,X,X,n,
+        X,O,O,X,n,
+        X,X,X,X,n,
+        X,O,O,O,n,
+        X,O,O,O,n
+    ],
+    "q": [
+        X,X,X,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,O,X,X,n,
+        X,X,X,X,n
+    ],
+    "r": [
+        X,X,X,X,n,
+        X,O,O,X,n,
+        X,X,X,X,n,
+        X,O,X,O,n,
+        X,O,O,X,n
+    ],
+    "s": [
+        O,X,X,X,n,
+        X,O,O,O,n,
+        O,X,X,O,n,
+        O,O,O,X,n,
+        X,X,X,O,n
+    ],
+    "t": [
+        X,X,X,X,n,
+        O,X,X,O,n,
+        O,X,X,O,n,
+        O,X,X,O,n,
+        O,X,X,O,n
+    ],
+    "u": [
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,X,X,X,n
+    ],
+    "v": [
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,O,O,X,n,
+        O,X,X,O,n,
+        O,X,X,O,n
+    ],
+    "w": [
+        X,O,O,X,n,
+        X,O,O,X,n,
+        X,X,X,X,n,
+        X,X,X,X,n,
+        X,O,O,X,n
+    ],
+    "x": [
+        X,O,O,X,n,
+        X,O,O,X,n,
+        O,X,X,O,n,
+        X,O,O,X,n,
+        X,O,O,X,n
+    ],
+    "y": [
+        X,O,O,X,n,
+        X,O,O,X,n,
+        O,X,X,O,n,
+        O,X,X,O,n,
+        O,X,X,O,n
+    ],
+    "z": [
+        X,X,X,X,n,
+        O,O,O,X,n,
+        O,X,X,O,n,
+        X,O,O,O,n,
+        X,X,X,X,n
+    ],
     // "0": [
 
     // ],
