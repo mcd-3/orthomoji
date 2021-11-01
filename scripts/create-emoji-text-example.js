be94f2e1880dd5860c79ce7bab15809b5edf25db
mcd-3 <matthew.carvalho.dagenais@gmail.com>
2021-10-31 23:28:18 -0400

    Removed example scripts

--- scripts/create-emoji-text-example.js
+++
@@ -1,9 +0,0 @@
-import { Orthomoji } from '../tools/generator.js';
-
-// Try it using `npm run orthomoji-example`
-// Change whatever you'd like
-const orthomoji = new Orthomoji();
-orthomoji
-    .setText('Hello Orthomoji!')
-    .setEmoji('😀')
-    .generate('./build/')
